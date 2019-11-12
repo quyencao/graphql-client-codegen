1247dd3b20aaf91328b5ad6f586bb720b05d8cf7
quyencao <boy20195@gmail.com>
2019-11-12 17:12:31 +0700

    init commit

---
+++ graphql/mutations/UpdateTodo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ApolloMutation
+      :mutation="require('../gqls/UpdateTodo.gql')"
+      :variables="{
+        
+      }"
+    >
+      <template slot-scope="{ mutate }">
+        <button v-on:click="mutate()">
+          Click Me
+        </button>
+      </template>
+    </ApolloMutation>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
